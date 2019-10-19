3af8dea7b955705f5129d319565e74861e65c057
lucashowardmiller <Lucas.h.miller@ndsu.edu>
2019-10-19 18:34:36 -0500

    Added URL validation in interactive

--- scrape.py
+++ scrape.py
@@ -23,6 +23,10 @@ if __name__ == '__main__':
     parser.add_argument("-c", "--colors", help="Nice colors, what's not to love?",
                         action="store_true")
 
+    # Future Features
+    # parser.add_argument("-o", "--out-file", help="Stores the results of operation in a text file")
+
+
     args = parser.parse_args()
 
     # Scraping logic
@@ -30,10 +34,14 @@ if __name__ == '__main__':
         print(ascii_art)
         print("v0.01 Extremely limited version.")
 
-    # add validation
-    # something like the following should work
-    # validators.url("http://google.com")
-    entry = input("Enter a web page to scrape for comments: ")
+    while True:
+        entry = input("Enter a web page to scrape for comments: ")
+        if not validators.url(entry):
+            print("Error parsing URL.")
+            continue
+        else:
+            break
+
     results = scrape_page.return_page_comments(entry)
     for result in results:
         print(result)

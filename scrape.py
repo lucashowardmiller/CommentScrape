be605ae7b3d18593f4a25983177f44c8e9c1bc10
lucashowardmiller <Lucas.h.miller@ndsu.edu>
2019-10-20 13:44:20 -0500

    Added skip arg to wrap_print, added save file

--- scrape.py
+++ scrape.py
@@ -6,9 +6,12 @@ from colorama import Fore, init
 
 """Sample Driver for the Comment Scrape Library."""
 
-# Global Args for file output / color
-
+# Global Args for color / file output / quiet
 USE_COLOR = False
+SAVE_TO_FILE = False
+FILE_REFERENCE = ""
+QUIET = False
+
 
 ascii_art = """<!--
  _____                                      _   _____                                
@@ -21,11 +24,14 @@ ascii_art = """<!--
                                                                     |_|        -->"""
 
 
-def wrap_print(print_string: str, color="Fore.WHITE"):
+def wrap_print(print_string: str, color=Fore.WHITE, skip=False):
     """Wraps the print function and adds colors, will later be used for everything"""
+    if skip and QUIET:
+        return
+    if SAVE_TO_FILE and not skip:
+        FILE_REFERENCE.write(f'{print_string}\n')
     if USE_COLOR:
-        # TODO fix eval, it's all trusted input. But still.
-        print(f'{eval(color)}{print_string}{Fore.RESET}')
+        print(f'{color}{print_string}{Fore.RESET}')
     else:
         print(print_string)
 
@@ -37,16 +43,21 @@ if __name__ == '__main__':
     parser.add_argument("-q", "--quiet", help="Silences header art and row names", action="store_true")
     parser.add_argument("-t", "--target", help="Enter a target URL to start the scan")
     parser.add_argument("-c", "--color", help="Nice colors, what's not to love?", action="store_true")
+    parser.add_argument("-o", "--out", help="Stores the results of operation in a text file")
 
     # Future Features
     # parser.add_argument("-r", "--rank-results", help="Returns the found comments, ranked by potential")
-    # parser.add_argument("-o", "--out-file", help="Stores the results of operation in a text file")
     # parser.add_argument("-s", "--show-source", help="Shows the url source page for each comment", action="store_true")
     # parser.add_argument("-i", "--interactive", help="Interactive mode")
 
     args = parser.parse_args()
 
+    # Global var setup
     USE_COLOR = args.color
+    QUIET = args.quiet
+    if args.out:
+        SAVE_TO_FILE = True
+        FILE_REFERENCE = open(args.out, "w+")
 
     # Windows printing support for Colorama
     init()
@@ -55,26 +66,29 @@ if __name__ == '__main__':
         if validators.url(args.target):
             entry = args.target
         else:
-            wrap_print("Could not parse URL for -t/--target", color="Fore.RED")
-            wrap_print("Exiting...", color="Fore.RED")
+            wrap_print("Could not parse URL for -t/--target", color=Fore.RED)
+            wrap_print("Exiting...", color=Fore.RED)
             sys.exit(1)
 
-    if not args.quiet:
-        wrap_print(f'{ascii_art}', color="Fore.BLUE")
+    wrap_print(f'{ascii_art}', color=Fore.BLUE, skip=True)
 
     if not args.target:
         while True:
             entry = input("Enter a web page to scrape for comments: ")
             if not validators.url(entry):
-                wrap_print("Error parsing URL. Try Again.", color="Fore.RED")
+                wrap_print("Error parsing URL. Try Again.", color=Fore.RED)
                 continue
             else:
                 break
 
     results = scrape_page.return_page_comments(entry)
+    wrap_print("Scraping Results:", color=Fore.CYAN, skip=True)
     for result in results:
-        wrap_print(f'{result}', color="Fore.GREEN")
+        wrap_print(f'{result}', color=Fore.GREEN)
 
     # TODO Work out how to display findings, HI -> Low / By category of match?
 
+    # Cleanup
+    if SAVE_TO_FILE:
+        FILE_REFERENCE.close()
     pass

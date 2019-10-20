bee0739e90cfe965505f8531f86356a9517d4cd9
lucashowardmiller <Lucas.h.miller@ndsu.edu>
2019-10-19 23:43:33 -0500

    ASCII Art + TODO

--- scrape.py
+++ scrape.py
@@ -5,12 +5,19 @@ from colorama import Fore, init
 
 """Sample Driver for the Comment Scrape Library."""
 
-# This will be good at some point
-ascii_art = """Comment Scrape"""
-version_string = """v0.01 Extremely limited version."""
+ascii_art = """<!--
+ _____                                      _   _____                                
+/  __ \                                    | | /  ___|                               
+| /  \/ ___  _ __ ___  _ __ ___   ___ _ __ | |_\ `--.  ___ _ __ __ _ _ __   ___ _ __ 
+| |    / _ \| '_ ` _ \| '_ ` _ \ / _ \ '_ \| __|`--. \/ __| '__/ _` | '_ \ / _ \ '__|
+| \__/\ (_) | | | | | | | | | | |  __/ | | | |_/\__/ / (__| | | (_| | |_) |  __/ |   
+ \____/\___/|_| |_| |_|_| |_| |_|\___|_| |_|\__\____/ \___|_|  \__,_| .__/ \___|_|   
+ v0.01 An extremely limited version.                                | |              
+                                                                    |_|        -->"""
 
 
-def save_to_file():
+def save_to_file(filename: str):
+    """Saves the results to a file (Formatted as text)"""
     pass
 
 
@@ -23,11 +30,11 @@ if __name__ == '__main__':
                         action="store_true")
     parser.add_argument("-s", "--show-source", help="Shows the url source page for each comment",
                         action="store_true")
-    # parser.add_argument("-c", "--color", help="Nice colors, what's not to love?",action="store_true")
     parser.add_argument("-t", "--target", help="Enter a target URL to start the scan")
     parser.add_argument("-i", "--interactive", help="Enter a target URL to start the scan")
 
     # Future Features
+    # parser.add_argument("-c", "--color", help="Nice colors, what's not to love?",action="store_true")
     # parser.add_argument("-r", "--rank-results", help="Returns the found comments, ranked by potential")
     # parser.add_argument("-o", "--out-file", help="Stores the results of operation in a text file")
 
@@ -36,7 +43,6 @@ if __name__ == '__main__':
     # Scraping logic
     if not args.quiet:
         print(ascii_art)
-        print(version_string + "\n")
 
     while True:
         entry = input("Enter a web page to scrape for comments: ")
@@ -49,5 +55,7 @@ if __name__ == '__main__':
     results = scrape_page.return_page_comments(entry)
     for result in results:
         print(result)
-        
+
+    # TODO Work out how to display findings, HI -> Low / By category of match?
+
     pass

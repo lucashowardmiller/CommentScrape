9e919a863fa8b5806a13b7ce8aa4191070c82bf8
lucashowardmiller <Lucas.h.miller@ndsu.edu>
2019-10-20 01:37:56 -0500

    Added support for -t/--target, args will be uncommented as added

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 from comment_scrape import scrape_page
 import argparse
 import validators
+import sys
 from colorama import Fore, init
 
 """Sample Driver for the Comment Scrape Library."""
@@ -22,35 +23,40 @@ def save_to_file(filename: str):
 
 
 if __name__ == '__main__':
-
     # Parse and set args
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-q", "--quiet", help="Silences header art and row names",
-                        action="store_true")
-    parser.add_argument("-s", "--show-source", help="Shows the url source page for each comment",
-                        action="store_true")
+    parser.add_argument("-q", "--quiet", help="Silences header art and row names", action="store_true")
     parser.add_argument("-t", "--target", help="Enter a target URL to start the scan")
-    parser.add_argument("-i", "--interactive", help="Enter a target URL to start the scan")
 
     # Future Features
     # parser.add_argument("-c", "--color", help="Nice colors, what's not to love?",action="store_true")
     # parser.add_argument("-r", "--rank-results", help="Returns the found comments, ranked by potential")
     # parser.add_argument("-o", "--out-file", help="Stores the results of operation in a text file")
+    # parser.add_argument("-s", "--show-source", help="Shows the url source page for each comment", action="store_true")
+    # parser.add_argument("-i", "--interactive", help="Interactive mode")
 
     args = parser.parse_args()
 
-    # Scraping logic
+    if args.target:
+        if validators.url(args.target):
+            entry = args.target
+        else:
+            print("Could not parse URL for -t/--target")
+            print("Exiting...")
+            sys.exit(1)
+
     if not args.quiet:
         print(ascii_art)
 
-    while True:
-        entry = input("Enter a web page to scrape for comments: ")
-        if not validators.url(entry):
-            print("Error parsing URL. Try Again.")
-            continue
-        else:
-            break
+    if not args.target:
+        while True:
+            entry = input("Enter a web page to scrape for comments: ")
+            if not validators.url(entry):
+                print("Error parsing URL. Try Again.")
+                continue
+            else:
+                break
 
     results = scrape_page.return_page_comments(entry)
     for result in results:
